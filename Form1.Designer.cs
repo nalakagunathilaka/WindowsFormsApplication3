2b1b26db7c6438bd116e068bbb24d59dcbeea7b0
nalakagunathilaka <nalakagunathilaka@gmail.com>
2017-06-26 08:44:32 +0530

    sales ui

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Addbttn = new System.Windows.Forms.Button();
+            this.MASBttn = new System.Windows.Forms.Button();
+            this.Totaltxt = new System.Windows.Forms.TextBox();
+            this.Total = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.quantitytxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Discount = new System.Windows.Forms.Label();
+            this.dscnttxt = new System.Windows.Forms.TextBox();
+            this.CLRBttn = new System.Windows.Forms.Button();
+            this.RMVALLBttn = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.pnametxt = new System.Windows.Forms.TextBox();
+            this.pcodetxt = new System.Windows.Forms.TextBox();
+            this.Remove = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Endtotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Remove,
+            this.product,
+            this.quantity,
+            this.price,
+            this.Endtotal});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 258);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(588, 144);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // Addbttn
+            // 
+            this.Addbttn.Location = new System.Drawing.Point(31, 206);
+            this.Addbttn.Name = "Addbttn";
+            this.Addbttn.Size = new System.Drawing.Size(99, 32);
+            this.Addbttn.TabIndex = 3;
+            this.Addbttn.Text = "ADD";
+            this.Addbttn.UseVisualStyleBackColor = true;
+            this.Addbttn.Click += new System.EventHandler(this.Addbttn_Click);
+            // 
+            // MASBttn
+            // 
+            this.MASBttn.Location = new System.Drawing.Point(31, 422);
+            this.MASBttn.Name = "MASBttn";
+            this.MASBttn.Size = new System.Drawing.Size(99, 31);
+            this.MASBttn.TabIndex = 5;
+            this.MASBttn.Text = "FINISH";
+            this.MASBttn.UseVisualStyleBackColor = true;
+            this.MASBttn.Click += new System.EventHandler(this.MASBttn_Click);
+            // 
+            // Totaltxt
+            // 
+            this.Totaltxt.Location = new System.Drawing.Point(246, 427);
+            this.Totaltxt.Name = "Totaltxt";
+            this.Totaltxt.Size = new System.Drawing.Size(110, 20);
+            this.Totaltxt.TabIndex = 7;
+            this.Totaltxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Totaltxt.TextChanged += new System.EventHandler(this.Totaltxt_TextChanged);
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(178, 430);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(47, 13);
+            this.Total.TabIndex = 8;
+            this.Total.Text = "Total Bill";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Quantity";
+            // 
+            // quantitytxt
+            // 
+            this.quantitytxt.Location = new System.Drawing.Point(470, 168);
+            this.quantitytxt.Name = "quantitytxt";
+            this.quantitytxt.Size = new System.Drawing.Size(111, 20);
+            this.quantitytxt.TabIndex = 11;
+            this.quantitytxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.quantitytxt.TextChanged += new System.EventHandler(this.quantitytxt_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Product code";
+            // 
+            // Discount
+            // 
+            this.Discount.AutoSize = true;
+            this.Discount.Location = new System.Drawing.Point(590, 171);
+            this.Discount.Name = "Discount";
+            this.Discount.Size = new System.Drawing.Size(49, 13);
+            this.Discount.TabIndex = 14;
+            this.Discount.Text = "Discount";
+            // 
+            // dscnttxt
+            // 
+            this.dscnttxt.Location = new System.Drawing.Point(642, 168);
+            this.dscnttxt.Name = "dscnttxt";
+            this.dscnttxt.Size = new System.Drawing.Size(92, 20);
+            this.dscnttxt.TabIndex = 15;
+            this.dscnttxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.dscnttxt.TextChanged += new System.EventHandler(this.dscnttxt_TextChanged);
+            // 
+            // CLRBttn
+            // 
+            this.CLRBttn.Location = new System.Drawing.Point(181, 206);
+            this.CLRBttn.Name = "CLRBttn";
+            this.CLRBttn.Size = new System.Drawing.Size(99, 33);
+            this.CLRBttn.TabIndex = 16;
+            this.CLRBttn.Text = "CLEAR";
+            this.CLRBttn.UseVisualStyleBackColor = true;
+            this.CLRBttn.Click += new System.EventHandler(this.CLRBttn_Click);
+            // 
+            // RMVALLBttn
+            // 
+            this.RMVALLBttn.Location = new System.Drawing.Point(637, 258);
+            this.RMVALLBttn.Name = "RMVALLBttn";
+            this.RMVALLBttn.Size = new System.Drawing.Size(97, 32);
+            this.RMVALLBttn.TabIndex = 17;
+            this.RMVALLBttn.Text = "REMOVE ALL";
+            this.RMVALLBttn.UseVisualStyleBackColor = true;
+            this.RMVALLBttn.Click += new System.EventHandler(this.RMVALLBttn_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(31, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(703, 149);
+            this.dataGridView2.TabIndex = 18;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // pnametxt
+            // 
+            this.pnametxt.Location = new System.Drawing.Point(109, 168);
+            this.pnametxt.Name = "pnametxt";
+            this.pnametxt.Size = new System.Drawing.Size(103, 20);
+            this.pnametxt.TabIndex = 19;
+            this.pnametxt.TextChanged += new System.EventHandler(this.pnametxt_TextChanged);
+            this.pnametxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pnametxt_KeyPress);
+            // 
+            // pcodetxt
+            // 
+            this.pcodetxt.Location = new System.Drawing.Point(306, 167);
+            this.pcodetxt.Name = "pcodetxt";
+            this.pcodetxt.Size = new System.Drawing.Size(104, 20);
+            this.pcodetxt.TabIndex = 20;
+            this.pcodetxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.pcodetxt.TextChanged += new System.EventHandler(this.pcodetxt_TextChanged);
+            this.pcodetxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pcodetxt_KeyPress);
+            // 
+            // Remove
+            // 
+            this.Remove.HeaderText = "Remove";
+            this.Remove.Name = "Remove";
+            this.Remove.Text = "Remove";
+            this.Remove.UseColumnTextForButtonValue = true;
+            // 
+            // product
+            // 
+            this.product.HeaderText = "Product";
+            this.product.Name = "product";
+            // 
+            // quantity
+            // 
+            this.quantity.HeaderText = "Quantity";
+            this.quantity.Name = "quantity";
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Price";
+            this.price.Name = "price";
+            // 
+            // Endtotal
+            // 
+            this.Endtotal.HeaderText = "Total";
+            this.Endtotal.Name = "Endtotal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 480);
+            this.Controls.Add(this.pcodetxt);
+            this.Controls.Add(this.pnametxt);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.RMVALLBttn);
+            this.Controls.Add(this.CLRBttn);
+            this.Controls.Add(this.dscnttxt);
+            this.Controls.Add(this.Discount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.quantitytxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.Totaltxt);
+            this.Controls.Add(this.MASBttn);
+            this.Controls.Add(this.Addbttn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "                                                                                 " +
+    "           Make A Sale";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Addbttn;
+        private System.Windows.Forms.Button MASBttn;
+        private System.Windows.Forms.TextBox Totaltxt;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox quantitytxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Discount;
+        private System.Windows.Forms.TextBox dscnttxt;
+        private System.Windows.Forms.Button CLRBttn;
+        private System.Windows.Forms.Button RMVALLBttn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox pnametxt;
+        private System.Windows.Forms.TextBox pcodetxt;
+        private System.Windows.Forms.DataGridViewButtonColumn Remove;
+        private System.Windows.Forms.DataGridViewTextBoxColumn product;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Endtotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form 
+    {   
+        
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+        DataTable dt = new DataTable();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //fillCombo();
+            
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillData();
+            Addbttn.Enabled = false;
+            RMVALLBttn.Enabled = false;
+        }
+       
+
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.DataSource == null)
+            {
+                Addbttn.Enabled = false;
+                
+
+            }
+            if(dataGridView1.Columns[e.ColumnIndex].Name == "Remove"){
+                dataGridView1.Rows.RemoveAt(this.dataGridView1.Rows[e.RowIndex].Index);
+
+            }
+
+            
+
+
+        }
+        private void fillData()
+        {
+            
+    
+            //MySqlCommand cmd;
+            string selectQuery = "SELECT * FROM sampleinventory.inventory_tb ";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, connection);
+            //cmd = new MySqlCommand(selectQuery, connection);
+            //adapter.SelectCommand = cmd;
+            adapter.Fill(dt);
+            dataGridView2.DataSource = dt;
+
+
+        }
+
+        private void Addbttn_Click(object sender, EventArgs e)
+        {
+            string selectedpname;
+            string selectedprice;
+            string total;
+            string valuewithoutdiscount;
+            selectedpname = Convert.ToString(dataGridView2.CurrentRow.Cells[1].Value);
+            selectedprice = Convert.ToString(dataGridView2.CurrentRow.Cells[3].Value);
+            valuewithoutdiscount = Convert.ToString(Convert.ToDouble(selectedprice)*Convert.ToDouble(quantitytxt.Text));
+            total = Convert.ToString(Convert.ToDouble(valuewithoutdiscount) - ((Convert.ToDouble(dscnttxt.Text) / 100) * Convert.ToDouble(valuewithoutdiscount)));
+            dataGridView1.Rows.Add(" ", selectedpname, quantitytxt.Text, selectedprice, total );
+            RMVALLBttn.Enabled = true;
+            addPrice();
+       
+        }
+
+        
+
+        private void Totaltxt_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+ 
+        }
+        void addPrice()
+        {
+            double sum = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+            {
+                sum += Convert.ToDouble(dataGridView1.Rows[i].Cells[4].Value);
+            }
+            Totaltxt.Text = sum.ToString();
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void quantitytxt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView2.Rows[index];
+            pnametxt.Text = selectedRow.Cells[1].Value.ToString();
+            string selectedpcode;
+            selectedpcode = Convert.ToString(dataGridView2.CurrentRow.Cells[0].Value);
+            pcodetxt.Text = selectedpcode;
+        }
+
+
+        private void pnametxt_TextChanged(object sender, EventArgs e)
+        {
+            
+                DataView dv = new DataView(dt);
+                dv.RowFilter = string.Format("product LIKE '{0}%'", pnametxt.Text);
+                dataGridView2.DataSource = dv;
+                
+            
+        }
+
+        private void pcodetxt_TextChanged(object sender, EventArgs e)
+        {
+            
+                DataView dv = new DataView(dt);
+                dv.RowFilter = string.Format("CONVERT(`Product Code`, 'System.String') LIKE '{0}%'", pcodetxt.Text);
+                dataGridView2.DataSource = dv;
+            
+            
+        }
+
+        private void pnametxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            pcodetxt.Enabled = false;
+            
+        }
+
+        private void pcodetxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            pnametxt.Enabled = false;
+        }
+
+        private void CLRBttn_Click(object sender, EventArgs e)
+        {
+            pnametxt.Text = null;
+            pcodetxt.Text = null;
+            quantitytxt.Text = null;
+            dscnttxt.Text = null;
+            pcodetxt.Enabled = true;
+            pnametxt.Enabled = true;
+            Addbttn.Enabled = false;
+            //RMVALLBttn.Enabled = false;
+                
+        }
+
+        private void RMVALLBttn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            Totaltxt.Text = null;
+        }
+
+        private void dscnttxt_TextChanged(object sender, EventArgs e)
+        {
+            if (quantitytxt.Text == "0" && quantitytxt.Text == null && pnametxt.Text == null && pcodetxt.Text == null && dscnttxt.Text == null)
+            {
+                Addbttn.Enabled = false;
+                MessageBox.Show("Quantity cannot be zero");
+                pcodetxt.Text = null;
+                pnametxt.Text = null;
+                quantitytxt.Text = null;
+                MessageBox.Show("p");
+
+            }
+            else
+            {
+                Addbttn.Enabled = true;
+            }
+        }
+
+        private void MASBttn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+       
+        /*string getTotal()
+        {
+            int val = 0;
+            string value;
+            val = (Convert.ToInt32(dataGridView1.CurrentRow.Cells[2].Value)) * (Convert.ToInt32(dataGridView1.CurrentRow.Cells[3].Value));
+            value = val.ToString();
+            return value;
+        }*/
+        
+
+        
+    }
+}
